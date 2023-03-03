1a3a0f7940fa78e1d736092ca83f2c444be7eb79
Md Shamim Reza <opt.shamim@gmail.com>
2023-03-03 23:18:06 +0600

    crud complete

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+
+from .forms import RoomForm
 
 rooms = [
 	{'id': 1, 'name': 'Let learn the python!'},
@@ -27,5 +29,34 @@ def room(request, pk):
 
 
 def createRoom(request):
-	context = {}
+	form = RoomForm()
+	if request.method == 'POST':
+		form = RoomForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('home')
+
+	context = {'form': form}
 	return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+	room = Room.objects.get(id=pk)
+	form = RoomForm(instance=room)
+
+	if request.method == 'POST':
+		form = RoomForm(request.POST, instance=room)
+		if form.is_valid():
+			form.save()
+			return redirect('home')
+
+	context = { 'form': form }
+	return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+	room = Room.objects.get(id=pk)
+	if request.method == 'POST':
+		room.delete()
+		return redirect('home')
+	return render(request, 'base/delete.html', {'obj': room})
